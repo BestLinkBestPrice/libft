a15fbe87dc2042749f8973182023f217426e1702
Nils Bonnet <nbonnet@c1r5s8.42lausanne.ch>
2024-10-02 17:15:45 +0200

    efkusgf

--- ft_strchr.c
+++ ft_strchr.c
@@ -17,9 +17,9 @@ char	*ft_strchr(const char *s, int c)
 	i = 0;
 	while (s[i] != '\0')
 	{
-		if (s[i] == c)
+		if (s[i] == (char)c)
 		{
-			return ((char) s + i);
+			return (char *) (s + i);
 		}
 		i++;
 	}
